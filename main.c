509db0092f37b9085d1ec81a5a41fb6d22a8d1de
Abdelhamid Errahmouni <abdelhamid.errahmouni@gmail.com>
2023-12-06 00:21:51 +0100

    refined by me

--- main.c
+++ main.c
@@ -5,8 +5,9 @@ int main()
     int capital = 6;
     int product_price = 6;
     int profit = 0;
-    int total_products_sold = 0;
-    int iterations = 0;
+    int profit_to_gain = 2;
+    int total_products_to_sell = capital / product_price;
+    int iterations = 1;
 
     while (capital < 60)
     {
@@ -15,21 +16,21 @@ int main()
         {
             int num_products_bought = profit / product_price;
             capital += num_products_bought * product_price;  // Use the profit to buy products
-            total_products_sold += num_products_bought;  // Accumulate the number of products sold
+            total_products_to_sell += num_products_bought;  // Accumulate the number of products sold
             profit %= product_price;  // Update profit after buying products
 
-            printf("Iteration: %d - Bought %d products.\n", iterations, num_products_bought);
+            printf("Iteration: %d - Bought %d products\n", iterations, num_products_bought);
         }
         else
         {
             // If there's not enough profit to buy a product, generate more profit
-            profit += 2 * (capital / product_price);
+            profit += profit_to_gain * (capital / product_price);
             printf("Iteration: %d - Generated more profit.\n", iterations);
         }
 
         iterations++;
 
-        printf("Products Sold So Far: %d\n", total_products_sold);
+        printf("Total Products to sell: %d\n", total_products_to_sell);
         printf("Profit: %d\n", profit);
         printf("Capital: %d\n", capital);
         printf("-----------------------------\n\n");
@@ -39,7 +40,7 @@ int main()
     printf("Iterations: %d\n", iterations);
     printf("Final Profit: %d\n", profit);
     printf("Final Capital: %d\n", capital);
-    printf("Total Products Sold: %d\n", total_products_sold);
+    printf("Total Products Sold: %d\n", total_products_to_sell);
 
     return 0;
 }
